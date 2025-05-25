2967233d390240b30f4f6b43e97a983442bcda6e
lycheegreentea <laurenkchen@gmail.com>
2025-05-25 16:26:13 -0400

    still broken

---
+++ Reverie/FavoriteManager.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteManager.swift
+//  Reverie
+//
+//  Created by Lauren Chen on 5/25/25.
+//
+
+import Foundation
--- Reverie/FavoriteWords.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FavoriteWords.swift
-//  Reverie
-//
-//  Created by Lauren Chen on 5/24/25.
-//
-
-import Foundation

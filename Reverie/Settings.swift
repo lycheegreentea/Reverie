d79888f6dc7f7731710ffec9d377cf277f775058
lycheegreentea <laurenkchen@gmail.com>
2025-06-04 22:44:17 -0400

    light/dark mode

---
+++ Reverie/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Reverie
+//
+//  Created by Lauren Chen on 6/4/25.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}

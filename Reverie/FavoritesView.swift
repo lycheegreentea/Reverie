a1dbaf510c1351c6f5964da598a7c89d0d5c7ac8
lycheegreentea <laurenkchen@gmail.com>
2025-06-05 18:06:54 -0400

    eeeeeee

---
+++ Reverie/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Reverie
+//
+//  Created by Lauren Chen on 6/5/25.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
